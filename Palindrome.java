92a3c5ef6a22dc7262291dc7258b2937d3ae7f4c
Javid Guliyev <>
2021-04-02 13:01:49 +0000

    Palindrome

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,17 @@
 public class Palindrome{
     public static boolean is_palindrome(String s)
     {
-        // add your code here
-        return true;
+
+    String clean = s.replaceAll("\\s+", "").toLowerCase();
+    int length = clean.length();
+    int forward = 0;
+    int backward = length - 1;
+    while (backward > forward) {
+        char forwardChar = clean.charAt(forward++);
+        char backwardChar = clean.charAt(backward--);
+        if (forwardChar != backwardChar)
+            return false;
+    }
+    return true;
     }
 }
